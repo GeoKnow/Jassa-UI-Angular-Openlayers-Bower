ad3cb709f8c873229434911004e35c2ef1eff931
cstadler <cstadler@informatik.uni-leipzig.de>
2014-09-14 23:57:53 +0200

    Updating version 0.0.4-SNAPSHOT with tag v0.0.4-SNAPSHOT

--- jassa-ui-angular-openlayers.js
+++ jassa-ui-angular-openlayers.js
@@ -2,10 +2,10 @@
  * jassa-ui-angular
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.0.4-SNAPSHOT - 2014-08-19
+ * Version: 0.0.4-SNAPSHOT - 2014-09-14
  * License: MIT
  */
-angular.module("ui.jassa.openlayers", ["ui.jassa.openlayers.jassa-map-ol","ui.jassa.openlayers.jassa-map-ol-a"]);
+angular.module("ui.jassa.openlayers", ["ui.jassa.openlayers.jassa-map-ol"]);
 //TODO Move to some better place
 jassa.setOlMapCenter = function(map, config) {
     var zoom = config.zoom;
@@ -509,7 +509,8 @@ $.widget('custom.ssbMap', {
             visibility: true,
             displayInLayerSwitcher: true,
             styleMap: new OpenLayers.StyleMap({'default': new OpenLayers.Style(this.styles.markerStyle)}),
-            renderers: renderer
+            renderers: renderer,
+			strategies: [new OpenLayers.Strategy.Cluster()]
         });
 
         // TODO Make it easy to exchange the URL pattern
@@ -1230,156 +1231,3 @@ $.widget('custom.ssbMap', {
 });
 
 })(jQuery);
-
-// Legacy version - don't use if you don't have to
-
-angular.module('ui.jassa.openlayers.jassa-map-ol-a', [])
-
-.controller('JassaMapOlACtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-        
-        var refresh;
-        
-        var defaultViewStateFetcher = new Jassa.geo.ViewStateFetcher();
-
-        // Make Jassa's ObjectUtils known to the scope - features the hashCode utility function
-        $scope.ObjectUtils = Jassa.util.ObjectUtils;
-        
-        
-        $scope.$watch('config', function(config, oldConfig) {
-            //console.log('Config update: ', config);
-            
-            if(_(config).isEqual(oldConfig)) {
-                return;
-            }
-            
-            //console.log('Compared: ' + JSON.stringify(config) + ' -> ' + JSON.stringify(oldConfig));
-            
-            Jassa.setOlMapCenter($scope.map, config);
-        }, true);
-        
-
-        var watchList = '[map.center, map.zoom, ObjectUtils.hashCode(sources)]'; //viewStateFetcher
-        
-        $scope.$watch(watchList, function() {
-            //console.log('Map refresh: ' + Jassa.util.ObjectUtils.hashCode($scope.config));
-            refresh();
-        }, true);
-        
-        
-        refresh = function() {
-             
-            var mapWrapper = $scope.map.widget;
- 
-            mapWrapper.clearItems();
-
-            var dataSources = $scope.sources;
-            
-            
-            var bounds = Jassa.geo.openlayers.MapUtils.getExtent($scope.map);
-            
-            _(dataSources).each(function(dataSource) {
-
-                var viewStateFetcher = dataSource.viewStateFetcher || defaultViewStateFetcher;
-                
-                var sparqlService = dataSource.sparqlService;
-                var mapFactory = dataSource.mapFactory;
-                //var conceptFactory = dataSource.conceptFactory
-                var conceptFactory = dataSource.conceptFactory;
-                var concept = conceptFactory.createConcept();
-                
-                var quadTreeConfig = dataSource.quadTreeConfig;
-                
-                var promise = viewStateFetcher.fetchViewState(sparqlService, mapFactory, concept, bounds, quadTreeConfig);
-                
-                // TODO How to obtain the marker style?
-                promise.done(function(viewState) {
-                    var nodes = viewState.getNodes();
-                    
-                    _(nodes).each(function(node) {
-                        //console.log('booooo', node);
-                        if(!node.isLoaded) {
-                            //console.log('box: ' + node.getBounds());
-                            mapWrapper.addBox('' + node.getBounds(), node.getBounds());
-                        }
-                        
-                        var data = node.data || {};
-                        var docs = data.docs || [];
-
-                        _(docs).each(function(doc) {
-                            var itemData = {
-                                id: doc.id,
-                                config: dataSource // Make the dataSource object part of the marker's data
-                            };
-
-							var wkt = doc.wkt.getLiteralLexicalForm();
-
-                            mapWrapper.addWkt(doc.id, wkt, itemData);// {fillColor: markerFillColor, strokeColor: markerStrokeColor});
-                            
-                        });
-                    });
-                });
-                
-                
-            });
-        };
-        
-}])
-
-//http://jsfiddle.net/A2G3D/1/
-.directive('jassaMapOlA', function($parse) {
-    return {
-        restrict: 'EA',
-        replace: true,
-        template: '<div></div>',
-        controller: 'JassaMapOlACtrl',
-        scope: {
-            config: '=',
-            sources: '=',
-            onSelect: '&select',
-            onUnselect: '&unselect'
-        },
-        link: function (scope, element, attrs) {
-            
-            var $el = jQuery(element).ssbMap();
-            var widget = $el.data('custom-ssbMap');
-
-            var map = widget.map;
-            map.widget = widget;
-            
-            scope.map = map;
-
-            Jassa.setOlMapCenter(scope.map, scope.config);
-
-            
-            var syncModel = function(event) {
-                var tmp = scope.map.getCenter();
-                var center = tmp.transform(scope.map.projection, scope.map.displayProjection);
-                
-                //console.log('syncModel', center);
-
-                scope.config.center = {lon: center.lon, lat: center.lat};
-                scope.config.zoom = scope.map.getZoom();
-                if(!scope.$root.$$phase) {
-                    scope.$apply();
-                }
-            };
-
-            
-            $el.on('ssbmapfeatureselect', function(ev, data) {
-                scope.onSelect({data: data});
-            });
-
-            $el.on('ssbmapfeatureunselect', function(ev, data) {
-                scope.onUnselect({data: data});
-            });
-                    
-            
-            map.events.register('moveend', this, syncModel);
-            map.events.register('zoomend', this, syncModel);
-        }
-            
-    };
-})
-
-;
-
