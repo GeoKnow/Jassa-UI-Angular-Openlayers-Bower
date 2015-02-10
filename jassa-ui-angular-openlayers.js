cedaf871a314e0718a6015ec53c1bd36399bde63
cstadler <cstadler@informatik.uni-leipzig.de>
2015-02-10 15:33:17 +0100

    Updating version 0.9.0-SNAPSHOT with tag v0.9.0-SNAPSHOT

--- jassa-ui-angular-openlayers.js
+++ jassa-ui-angular-openlayers.js
@@ -2,7 +2,7 @@
  * jassa-ui-angular
  * https://github.com/GeoKnow/Jassa-UI-Angular
 
- * Version: 0.9.0-SNAPSHOT - 2015-01-18
+ * Version: 0.9.0-SNAPSHOT - 2015-02-10
  * License: MIT
  */
 angular.module("ui.jassa.openlayers", ["ui.jassa.openlayers.jassa-map-ol"]);
